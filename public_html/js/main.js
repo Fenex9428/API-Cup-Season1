129b2987a770129605af580fdb72ff4b41c0f76e
Nikolay <asoman97@gmail.com>
2015-01-28 23:55:11 +0300

    Графика и скрипты
    Добавил спрайты и фон. Начал писать скриптовую часть. Реализованно:
    инициализация игроков, перемещение, функция игрокового цикла.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,7 +4,38 @@
  * and open the template in the editor.
  */
 
+var player1, player2;
+var StartPos = 325;
 
 $( window ).load(function() {
-console.log("hi");
+
+
+ player1 = $("#player1");
+ player2 = $("#player2");
+ 
+ player1.pos = 0;
+ player2.pos = 0;
+ player1.speed=10;
+ player2.speed=-10;
+
+update();
+
+setInterval(update, 100);
+
 });
+
+
+function move(player){
+    player.pos +=player.speed;
+    player.css("margin-top", StartPos+player.pos);
+};
+
+
+
+
+
+function update(){
+    move(player1);
+    move(player2);
+    
+};
